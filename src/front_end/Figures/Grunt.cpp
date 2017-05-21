32c1fa991ee1a644c54028170a03814d33487f5b
DMaitia <dariusmaitia@gmail.com>
2017-05-21 06:31:31 -0300

    vamos con SDL

--- src/front_end/Figures/Grunt.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by darius on 20/05/17.
-//
-
-#include "Grunt.h"
--- z_tests/tests_front_end/FEnd_test.cpp
+++ z_tests/tests_front_end/FEnd_test.cpp
@@ -3,13 +3,19 @@
 //
 
 #include <gtest/gtest.h>
-#include "../../src/header_for_tests.h"
-#include "../../src/front_end/Main_Window.h"
+#include "../../src/front_end/SDL_Interface/Main_Window.h"
 
+/*
 TEST(FrontEndTest,Main_Window){
   Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(my_argc, my_argv);
 
   Main_Window interface(my_argc,my_argv);
 
   app->run(*interface.get_main_window());
+}
+*/
+
+TEST(FrontEndTestWithSQL,Main_Window){
+  Main_Window main_window;
+  SDL_Delay(3000);
 }
