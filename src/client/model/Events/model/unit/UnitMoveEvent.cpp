6d2b2ed81c5e98a02173b0c70b35156d640aa12d
martinBosch <martinbosch17@gmail.com>
2017-06-27 13:06:45 -0300

    arreglado bug de unidad al moverse en el agua

--- src/client/model/Events/model/unit/UnitMoveEvent.cpp
+++ src/client/model/Events/model/unit/UnitMoveEvent.cpp
@@ -12,18 +12,20 @@ void UnitMoveEvent::process() {
   // La velocidad de la vista respecto al server
   float velView = 4.0;
   ViewPosition step(dist.getX()/velView, dist.getY()/velView);
-
-//  if (step.getX()<1.0 && step.getY()<1.0)
-
-  std::cout << "DIST" << std::endl;
-  std::cout << dist.getX() << " " << dist.getY() << std::endl;
-  std::cout << "STEP" << std::endl;
-  std::cout << step.getX() << " " << step.getY() << std::endl;
-
   ViewPosition unitViewPos = unitView->getViewPos();
-  for (int i=0; i < velView; i++) {
-    unitViewPos.add(step);
-    unitView->addMove(unitViewPos);
+
+  // Si el paso es menor a 0 en X e Y la unidad empieza a rotar en cada paso,
+  // lo que hace que se vea mal. Entonces se mueve primero en X y luego en Y.
+  if (step.getX()<1.0 && step.getY()<1.0) {
+    while (unitPos != posTo) {
+      unitPos.move(posTo);
+      unitView->addMove(ViewPosition(unitPos.getX(),unitPos.getY()));
+    }
+  } else {
+    for (int i=0; i < velView; i++) {
+      unitViewPos.add(step);
+      unitView->addMove(unitViewPos);
+    }
   }
 
   Map &map = model->getMap();
--- src/client/view/ViewPosition.cpp
+++ src/client/view/ViewPosition.cpp
@@ -28,6 +28,7 @@ double ViewPosition::getRotation(const ViewPosition &target) const {
   float deltaY = (-1*target.y) - (-1*this->y);
   float deltaX = target.x - this->x;
   double rotation = std::atan2(deltaY, deltaX) * 180.0000 / M_PI;
+
   return rotation/-1;
 }
 
@@ -40,22 +41,22 @@ int ViewPosition::getDrawRoration(const ViewPosition &target) const {
   if (rotation >= 0 && rotation < 22.5) {
     return 0;
   }
-  else if (rotation >= 22.5 && rotation < 67.5) {
+  else if (rotation > 22.5 && rotation <= 67.5) {
     return 45;
   }
-  else if (rotation >= 67.5 && rotation < 112.5) {
+  else if (rotation > 67.5 && rotation <= 112.5) {
     return 90;
   }
-  else if (rotation >= 112.5 && rotation < 157.5) {
+  else if (rotation > 112.5 && rotation <= 157.5) {
     return 135;
   }
-  else if (rotation >= 157.5 && rotation < 202.5) {
+  else if (rotation > 157.5 && rotation <= 202.5) {
     return 180;
   }
-  else if (rotation >= 202.5 && rotation < 247.5) {
+  else if (rotation > 202.5 && rotation <= 247.5) {
     return 225;
   }
-  else if (rotation >= 247.5 && rotation < 292.5) {
+  else if (rotation > 247.5 && rotation <= 292.5) {
     return 270;
   }
   else {
